612370d0a7066cffd9e09c59a2855e336ec9fa57
leandroleiteh <leandroluz201616@gmail.com>
2023-11-06 17:51:14 -0300

    feat: First Commit

---
+++ Ekan-dev-jr/src/main/java/com/ekan/dev/api/EkanDevJrApplication.java
@@ -0,0 +1,13 @@
+package com.ekan.dev.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkanDevJrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkanDevJrApplication.class, args);
+	}
+
+}
